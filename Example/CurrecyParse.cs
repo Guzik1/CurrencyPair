3ed99c9202340d6abe79a86ea1601f9821876ed2
Sebastian Guzik <guzikpack.kontakt@gmail.com>
2020-02-26 08:46:27 +0100

    Add example

---
+++ Example/CurrecyParse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CurrencyPair;
+
+namespace Example
+{
+    public class CurrecyParse
+    {
+        public void Example()
+        {
+            string currencyType = "Fiat";
+
+            CurrencyType type = CurrencyTypeExtension.Parse(currencyType);
+        }
+    }
+}
